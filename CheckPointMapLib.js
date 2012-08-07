25553398818a0dbb27f9e5707a2acdc0a684294d
Reda Al-Fardan <ibnkan@gmail.com>
2012-08-07 19:06:53 +0200

    updates and corrections
    layout, time formatting to Bahrain local time, location list updated etc

--- CheckPointMapLib.js
+++ CheckPointMapLib.js
@@ -4,6 +4,7 @@ var Bahrain = new google.maps.LatLng(26.125, 50.55);
 var markerIcon = "CheckPoint.png";
 var mapmarkers = [];
 
+
 //create an infoWindow object to be used by markers on the map
 var infoWindow = new google.maps.InfoWindow({
     maxWidth: 240
@@ -71,16 +72,39 @@ function hideInfo() {
     
 }
 //////////////////////////////////////////////////////////////////////
-function randomDecimal() {
-    var y = Math.random() - Math.random();
-    if (y <= 0) {
-        y = -1;
+
+function formatTime(str){
+    //get hours regex ie 2 digits not follwed by space(day) or end of the line(minutes)
+    var hours = parseInt(str.match(/\d{2}(?!$|\s)/), 10);
+    //add 3 hours for Bahrain timezone
+    hours += 3;
+    //correction for base 24h
+    if(hours >=24){
+        hours -=24;
+    }
+    
+    var ampm;
+    if (hours >= 12) {
+        ampm = " PM</strong>";
+        if (hours > 12) {
+            hours -= 12;
+        }
     }
     else {
-        y = 1;
+        ampm = " AM</strong>";
+        if (hours == 0) {
+            hours = 12;
+        }
     }
-    var x = y * (0.00008 + (Math.random() * 0.00008));
+    //put time in bold <stonrg> tag and underline
+    return str.replace(/\d{2}(?!$|\s)/,'@ <strong style="text-decoration:underline;">' + hours) + ampm;
     
+}
+//////////////////////////////////////////////////////////////////////
+var y = 1;
+function randomDecimal() {
+    y *= -1;
+    var x = y * (Math.random() * 0.0005);
     return x;
     
 }
@@ -129,7 +153,7 @@ function extractLatLng(text) {
 
 function PrintTweet(tweet) {
 
-    var htmltext = '<div style="background-color:GhostWhite"><img src="' + tweet[0] + '\" style="float:left; margin-bottom:20px; margin-right:10px;" /img><strong>' + tweet[1] + '</strong><br /><em style="font-size:75%;">' + tweet[2].replace(/(@|#)\w+/g, "") + '</em><br /><span style="font-size:55%;">posted @ ' + tweet[3] + '</span></div>';
+    var htmltext = '<div style="background-color:GhostWhite"><img src="' + tweet[0] + '\" style="float:left; margin-bottom:20px; margin-right:10px;" /img><strong>' + tweet[1] + '</strong><br /><em style="font-size:75%;">' + tweet[2].replace(/(@|#)\w+/g, "") + '</em><br /><span style="font-size:55%;">' + tweet[3] + '</span></div>';
 
     //document.getElementById("txt").innerHTML += htmltext;
 
@@ -146,11 +170,14 @@ function ProcessTweets(evt) {
 
     for (i = 0; i < response.results.length; i++) {
 
+        //create <a> tags for all links in a tweet using regexp $& = ($1,$2,$3...etc)
         var tweet = response.results[i].text.replace(/(\b(http|https):\/\/\S+)/g, '<a href="$&">$&</a>');
         //var tweeturl = extractURL(tweet);
+        //Add an @ sign to the profile name as done on twitter
         var profilename = '@' + response.results[i].from_user;
         var profileimage = response.results[i].profile_image_url;
-        var timestamp = response.results[i].created_at;
+        //remove seconds,year and timezone from created_at string using regexp then convert time to 12h format
+        var timestamp = formatTime(response.results[i].created_at.replace(/(:\d+\s\+\d+|\d{4})/gi,""));
 
         twitterdata[i] = [profileimage, profilename, tweet, timestamp];
 
