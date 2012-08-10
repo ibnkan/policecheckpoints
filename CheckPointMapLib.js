d3f44fded6f18fa89533c15fb9aa674cbc8926a5
Reda Al-Fardan <ibnkan@gmail.com>
2012-08-11 00:10:23 +0200

    timestamp layout

--- CheckPointMapLib.js
+++ CheckPointMapLib.js
@@ -112,22 +112,22 @@ function formatTime(str, j){
     var tt;
 
     if (d.getHours() >= 12) {
-        tt = " PM</strong>";
+        tt = " PM";
         if (d.getHours() > 12) {
             d.setHours(d.getHours() - 12);
         }
     }
     else {
-        tt = " AM</strong>";
+        tt = " AM";
         if (d.getHours() == 0) {
             d.setHours(12);
         }
     }
 
     //show local time in h:mm tt bold and underline, the (?:) if statement adds preceding zero for min [0-9]
-    timestamp [0] = '<strong>'+d.getHours() + ':' + (d.getMinutes()<10?'0':'') + d.getMinutes() + tt;
+    timestamp [0] = d.toLocaleString().substr(0, 3) + ', ' + d.toLocaleString().substr(8, 2)+ ' ' + d.toLocaleString().substr(4, 3);
     //then date in ddd, dd mmm in a new line
-    timestamp [0] += '<br />' + d.toLocaleString().substr(0, 3) + ', ' + d.toLocaleString().substr(8, 2)+ ' ' + d.toLocaleString().substr(4, 3);
+    timestamp [0] += ' @ ' + d.getHours() + ':' + (d.getMinutes()<10?'0':'') + d.getMinutes() + tt;
     
     return timestamp;
     
@@ -189,10 +189,10 @@ function PrintTweet(tweet) {
 
     var htmltext = '<div style="font-family: Tahoma;"><img src="' + tweet[0] + '\" style="height:28px; vertical-align: middle; border: lightgrey solid; border-width:1px; border-radius:4px;" /img>\n\
 <a href="https://twitter.com/' + tweet[1] + '\" title="User Profile"><strong> @' + tweet[1] + '</strong></a></div>\n\
-<div style="font-family: Tahoma; clear:both; background-color:GhostWhite; padding: 5px; margin-top: 2px; margin-bottom: 0; border-radius:8px;">\n\
+<div style="font-family: Tahoma; clear:both; background-color:GhostWhite; padding: 5px; margin-top: 4px; margin-bottom: 0px; border-radius:8px;">\n\
 <div style="font-size:75%; direction:rtl; text-align: right;">' + tweet[2].replace(/(@|#)\w+:*/g, "").replace(/["“”]/g,"").replace(/^:/g,"") + '</div>\n\
-<div style="font-size:60%; direction:ltr; text-align: left; margin:5px 0px; float:left;">' + tweet[3][0] + '</div>\n\
-<div style="font-size:60%; direction:rtl; text-align: right; margin:5px 0px; float:right;"><br />' + tweet[3][1] + '</div>\n\
+<div style="font-size:60%; direction:ltr; text-align: left;  margin-bottom:5px; margin-top: 8px; float:left;">' + tweet[3][0] + '</div>\n\
+<div style="font-size:60%; direction:rtl; text-align: right; margin-bottom:5px; margin-top: 8px; float:right;">' + tweet[3][1] + '</div>\n\
 </div>';
 
     return htmltext;
